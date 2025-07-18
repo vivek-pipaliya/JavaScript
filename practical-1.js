fd86c5bec45ac6ea21b408d0cbdbc366e959bf7d
vivek-pipaliya <vivekpialiya007@gmail.com>
2025-07-18 11:20:40 +0530

    store data in localstorage and display it onload

--- practical-1.js
+++ practical-1.js
@@ -3,7 +3,30 @@ const datainput = document.getElementById("datainput");  //take value form user 
 const show = document.getElementById("show");  // display input
 const addbutton = document.getElementById("addbutton");  //when user click the button it will add value in array
 
+
 let myarray = []; // created array for storing input
+
+window.onload = function(){
+    const valueBack = localStorage.getItem("myarray");  //taking values from local storage 
+    if(valueBack){
+    const valueGet = JSON.parse(valueBack); // converting the string into array
+    
+      for(let x = 0; x < valueGet.length; x++){
+        // console.log(myarray[x]);
+        let listHtml = ` <div class="form">
+                        <label class="radio">${valueGet[x]}
+                            <input class="input" type="checkbox" name="radio">
+                            <span class="checkmark"></span>
+                        </label>
+                        <button class="button">
+                            <img src="./images/delete.svg" alt="delete">
+                        </button>
+                    </div> `
+        document.getElementById("todolist").innerHTML += listHtml; //it will print the value which is stored in myarray var
+    }
+    }
+   
+}
 
 addbutton.addEventListener('click', function () {
     const valuetoadd = datainput.value; //created variable to store input 
@@ -22,17 +45,13 @@ addbutton.addEventListener('click', function () {
         document.getElementById("todolist").innerHTML += listHtml; //it will print the value which is stored in myarray var
     }
     // storing the array in local storage
-
-    let string = JSON.stringify(myarray);  // convert array in string
-
+    const string = JSON.stringify(myarray);  // convert array in string
     localStorage.setItem("myarray", string);   // storing the array/value in local storage
+});
 
-    let valueBack = localStorage.getItem("myarray");  //taking values from local storage 
-
-    let valueGet = JSON.parse(valueBack); // converting the string into array
-
-    console.log(valueGet)
-
-
-}); 
+// const deletebutton = document.getElementById("deletebutton");
+// const removeelement = document.getElementById("removeelement")
+// deletebutton.addEventListener('click', function)(){
+//     const removeitem = removeelement
+// }
 
