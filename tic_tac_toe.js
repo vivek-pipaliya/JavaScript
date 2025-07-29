3d6c8f9d95cd19289a0cbc663277e2edc3199611
vivek-pipaliya <vivekpialiya007@gmail.com>
2025-07-29 19:25:49 +0530

    tic tac toe full working done

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,5 +1,5 @@
 const buttons = document.querySelectorAll(".box")
-const reset = document.querySelectorAll(".restart")
+const reset = document.querySelector(".restart")
 
 let player1 = true;
 
@@ -13,12 +13,9 @@ const winning = [
     [0, 4, 8], [2, 4, 6]
 ];
 
-
 buttons.forEach(box => {
     box.addEventListener('click', function (event) {
-        // const buttonValue = event.target.value;
-        // console.log('Button clicked! Value:', buttonValue);
-        box.innerText = handle(); // give input as X or O.
+        box.innerText = turn(); // give input as X or O.
         box.style.padding = "20px 25px"; // after given true input it sets padding as per given.
         box.disabled = true; // it will not allow to change or re-enter X or O again.
         checkwinner(); // checks the winner logic.
@@ -26,7 +23,7 @@ buttons.forEach(box => {
 });
 
 // handle() function for checking the turn of playeer 
-function handle() {
+function turn() {
     if (player1) {  // checks if the player 1 is true then his turn
         player1 = false; //after complete of player 1 turn it will make it false and next time turn for palyer 2
         return "X"; //print/return X on button.
@@ -38,13 +35,40 @@ function handle() {
 
 function checkwinner() {
     for (let pattern of winning) {
-        // console.log('hello' ,pattern)
+        const pal1 = buttons[pattern[0]].innerText;
+        const pal2 = buttons[pattern[1]].innerText;
+        const pal3 = buttons[pattern[2]].innerText;
+        if (pal1 && pal1 === pal2 && pal2 === pal3 && pal3) {
+            document.getElementById("winner").innerHTML = pal1;
+            disablebox();
+        }
+    }
+    let draw = true;
+    for (let x = 0; x < buttons.length; x++) {
+        if (buttons[x].innerText === "") {
+            draw = false;
+        }
+    }
+    if (draw) {
+        document.getElementById("winner").innerHTML = "Draw";
+    }
+}
+const disablebox = () => {
+    for (let box of buttons) {
+        box.disabled = true;
     }
 }
 
-/* reset.forEach(restart => {
-    restart.addEventListener('click', function (event) {
-        const buttonValue = event.target.value;
+const enablebox = () => {
+    for (let box of buttons) {
+        box.disabled = false;
         box.innerText = "";
-    })
-    }) */
+        document.getElementById("winner").innerHTML = "";
+        box.style.padding = "30px 30px";
+    }
+}
+
+reset.addEventListener('click', () => {
+    player1 = true; // make the first turn activate
+    enablebox(); // after winner it will enable all buttons
+});
